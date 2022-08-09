a7436fa07a1e0ee7814b2edbaeab69b7278cd2f3
Tarcio Saraiva <tsaraiva@squareup.com>
2022-08-09 21:55:49 +1000

    move 'sigint handler' and 'server_socket' to Belldops

--- main.py
+++ main.py
@@ -11,24 +11,12 @@ import time
 HOST = '0.0.0.0'
 PORT = 9876
 
-# socket start
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-def handler(signum, frame):
-    print('\nCTRL-C was pressed. Terminating the program.\n')
-    camera.stop_recording()
-    camera.close()
-    server_socket.close()
-    call_light.off()
-    exit(1)
-
-signal.signal(signal.SIGINT, handler)
-
+# Class to handle speaking, playing
 class Sound():
     def __init__(self):
         self.data, self.fs = sf.read(self._get_ringbell_filename())
         print("Sound init'd")
-    
+
     def play_ring_tone(self):
         sd.play(self.data, self.fs)
         sd.wait()
@@ -37,6 +25,7 @@ class Sound():
         bell_num = random.choice([1, 2, 3])
         return '/home/pi/doorbell-{}.wav'.format(bell_num)
 
+# Class to handle camera
 class Camera:
     def __init__(self):
         self._handle = picamera.PiCamera()
@@ -52,29 +41,46 @@ class Camera:
     def record(self, cf):
         self._handle.start_recording(cf, format='h264')
 
+    def stop(self):
+        self._handle.stop_recording()
+        self._handle.close()
+
     def is_recording(self):
         return self._handle.recording
 
+# Main class
 class Belldops:
     def __init__(self):
         self._call_button = Button(12)
         self._call_light = LED(14)
         self._camera = Camera()
         self._sound = Sound()
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def _sigint_handler(self, signum, frame):
+        print('\nCTRL-C was pressed. Terminating the program.\n')
+
+        self._camera.stop()
+        self._call_light.off()
+
+        self._server_socket.close()
+        exit(1)
 
     def run(self):
         try:
+            signal.signal(signal.SIGINT, self._sigint_handler)
+
              # application loop
-            with server_socket:
-                server_socket.bind((HOST, PORT))
-                server_socket.listen()
+            with self._server_socket:
+                self._server_socket.bind((HOST, PORT))
+                self._server_socket.listen()
 
                 while True:
                     if self._call_button.is_pressed:
                         self._call_light.on()
                         self._sound.play_ring_tone()
 
-                        conn, addr = server_socket.accept()
+                        conn, addr = self._server_socket.accept()
                         print(f"Connected by {addr}")
                         cf = conn.makefile('wb')
 
